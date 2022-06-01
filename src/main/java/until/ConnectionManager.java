dc1ac0fbaf727cd97206c14980d0067562cbd184
Alex <sergdalm@gmail.com>
2022-06-01 12:44:43 +0300

    Added getPreparedStatement method

--- src/main/java/until/ConnectionManager.java
+++ src/main/java/until/ConnectionManager.java
@@ -2,13 +2,13 @@ package until;
 
 import lombok.SneakyThrows;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
+import java.sql.*;
 
 public class ConnectionManager {
     private static final String URL_KEY = "db.url";
     private static final String USER_KEY = "db.user";
     private static final String PASSWORD_KEY = "db.password";
+    private static Connection connection;
 
     static {
         loadDriver();
@@ -30,4 +30,56 @@ public class ConnectionManager {
             throw new RuntimeException(e);
         }
     }
+
+    public static PreparedStatement getPreparedStatement(String sql) {
+        ConnectionManager.connection = get();
+        PreparedStatement preparedStatement;
+        try {
+            preparedStatement = connection.prepareStatement(sql);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return preparedStatement;
+
+    }
+
+    public static PreparedStatement getPreparedStatementWithGeneratedKeys(String sql) {
+        ConnectionManager.connection = get();
+        PreparedStatement preparedStatement;
+        try {
+            preparedStatement = connection.prepareStatement(sql, PreparedStatement.RETURN_GENERATED_KEYS);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return preparedStatement;
+
+    }
+
+    public static Statement getStatement() {
+        ConnectionManager.connection = get();
+        try {
+            return connection.createStatement();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void closeConnection(PreparedStatement preparedStatement) {
+        try {
+            ConnectionManager.connection.close();
+            preparedStatement.close();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void closeConnection(Statement statement) {
+        try {
+            ConnectionManager.connection.close();
+            statement.close();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
 }
